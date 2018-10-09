fa40aa44436bda79b421835a37bad83b0355ee8d
Engit Prabhat <engitj@gmail.com>
2018-10-09 16:15:59 +0530

    Updated common module

--- src/common.rs
+++ src/common.rs
@@ -1,11 +1,5 @@
-use std::process;
-
-pub fn _fail_exit(code: i32) {
-    process::exit(code);
-}
-
 #[macro_export]
 macro_rules! fail_exit {
-    ($code:expr) => { common::_fail_exit($code) };
-    () => { common::_fail_exit(-1) };
+    ($code:expr) => { std::process::exit($code) };
+    () => { std::process::exit(-1) };
 }
